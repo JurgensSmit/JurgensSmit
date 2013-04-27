dec3248de0760246197cc34fbe2143de278fbe4c
Jurgens Smit <contact@jurgenssmit.com>
2013-04-27 15:52:48 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JurgensSmit::Application.config.secret_token = '613e9d6f925f1e003af9e67c320b14c62646a101ec792845b4af791ff3d66721a330cfb68c7e14f807559ba7684f02edbb1460d9824c35626f620cf7934b608e'
